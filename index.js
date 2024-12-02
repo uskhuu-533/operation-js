cdb290bfb53506708d4b4f519aaa9d8b25e61442
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-02 18:12:31 +0800

    commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const a = 6;
+const b = 4;
+const c = 5;
+console.log(a+b+c)
+
+const R = 10;
+const pi = 3.14;
+console.log(pi*R)
+
+const y2 = 2;
+const y1 = 6;
+const x2 = 3;
+const x1 = 1;
+const m = y2-y1/x2-x1;
+console.log(m)
+
+const data = 15;
+const time = 3;
+const bitRate = data/time;
+console.log(bitRate);
+
+const side1 =4 ;
+const side2 = 5;
+const side3 = 3;
+const p = (side1+side2+side3)/2
+const area = (p*(p-side1)*(p-side2)*(p-side3))**0.5;
+console.log(area);
+
+const C = 20;
+const F= 9/5*C+32;
+console.log(F);
+
+let teamA1 = 96;
+let teamA2 = 108;
+let teamA3 = 89;
+console.log((teamA1+teamA2+teamA3)/3)
+teamA1= 88;
+teamA2= 91;
+teamA3= 101;
+console.log((teamA1+teamA2+teamA3)/3)
+
+const sum = 25;
+const minius =5;
+const num1 = (sum+ minius)/2;
+const num2 = num1-5;
+console.log(num1, num2);
+
+const cm = 16;
+const inch = cm*0.394;
+console.log(inch) 
+
+const Rad = 15;
+const V = pi*Rad**2;
+console.log(V)
+console.log(2*pi*Rad)
+
+const num3 = 123;
+const modul= (num3/100)%1
+console.log(num3/100-modul)
+
